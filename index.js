3c48478eb2092a6518b7764496ff0e3d0ad6749e
Dhruv1701 <negidhruv1701@gmail.com>
2023-08-16 20:21:14 +0530

    Added redis cache

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Redis = require("ioredis");
 const express = require('express');
 const request = require('request');
 const cors = require('cors')
@@ -10,25 +11,36 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const client = new Redis(process.env.REDIS_URL);
+
 app.get('/',(req, res)=>{
     res.send("Server Start")
 })
 
-app.post(`/getNews`, (req, res) => {
+app.post(`/getNews`, async (req, res) => {
 
     const {country, category, page, pageSize} = req.body;
+
+    const cacheKey = `${country}-${category}-${page}-${pageSize}`;
+
+    const cacheValue = await client.get(cacheKey);
+    if (cacheValue) {
+        res.send(cacheValue);
+        return;
+    }
 
     const apiUrl = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&page=${page}&pageSize=${pageSize}&apiKey=${process.env.NEWS_API_KEY}`;
 
     const options = {
         url: apiUrl,
         headers: {
-            'User-Agent': 'News-Bulletin/1.0' // Replace with your app's identifier
+            'User-Agent': 'News-Bulletin/1.0'
         }
     };
 
-    request(options, (error, response, body) => {
+    request(options, async(error, response, body) => {
         if (!error && response.statusCode === 200) {
+            await client.set(cacheKey, body, 'EX', 7200);
             res.send(body);
         } else {
             res.status(response.statusCode).send(error);
